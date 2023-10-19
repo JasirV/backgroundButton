917ab49c7dafeb2cef89fb571747dfea30dcdb61
JasirV <jasirmon133@gmail.com>
2023-10-19 11:40:26 +0530

    first commit

---
+++ src/Components/Btn.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { Button } from 'react-bootstrap';
+const Btn = () => {
+    const [color,setcolor]=useState("black")
+const white=()=>{
+    setcolor((color==="black")?"white":"black");
+}
+  return (
+    <div  style={{ backgroundColor: color ,height:"749px"}}>
+        <Button onClick={white} className='m-5'>Click Me</Button>
+    </div>
+  )
+}
+
+export default Btn
